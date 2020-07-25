74721ff956c920144860625f1a0d4d3fe6cba49f
Falkreon <falkreon@gmail.com>
2020-07-25 13:35:05 -0500

    Flesh out MaterialAttribute and Material ABIs

--- src/main/java/com/playsawdust/chipper/glow/model/Material.java
+++ src/main/java/com/playsawdust/chipper/glow/model/Material.java
@@ -1,22 +1,107 @@
 package com.playsawdust.chipper.glow.model;
 
 import org.checkerframework.checker.nullness.qual.Nullable;
+import org.joml.Vector3d;
 
 public interface Material {
-	
 	/**
 	 * Gets an attribute of this Material
-	 * @param <T> The immutable type of the data represented by this MaterialAttribute
+	 * @param <T> The type of the data represented by this MaterialAttribute
 	 * @param attribute the MaterialAttribute to return a value for
 	 * @return the value of this MaterialAttribute, or null if no value is defined for this Material.
 	 */
-	public <T,U> @Nullable T getMaterialAttribute(MaterialAttribute<T, U> attribute);
+	public <T,U> @Nullable T getMaterialAttribute(MaterialAttribute<T> attribute);
 	
-	/**
-	 * Gets the value of a MaterialAttribute for this Material. For instance, getting its diffuse color as a Vec3
-	 * @param <U> The mutable type of data represented by this MaterialAttribute
-	 * @param attribute the MaterialAttribute to return a value for
-	 * @param result the object to place the value of this MaterialAttribute into; the object will be unchanged if no value is defined for this Material.
-	 */
-	public <T,U> void getMaterialAttribute(MaterialAttribute<T, U> attribute, U result);
+	public static class BlinnPhong implements Material {
+		private double specularity = 0.0;
+		private Vector3d diffuseColor = new Vector3d(1, 1, 1);
+		private double opacity = 1.0;
+		
+		private String diffuseTextureId;
+		private String normalTextureId;
+		private String specTextureId;
+		
+		@SuppressWarnings("unchecked")
+		@Override
+		public <T, U> @Nullable T getMaterialAttribute(MaterialAttribute<T> attribute) {
+			if (attribute==MaterialAttribute.DIFFUSE_COLOR) return (T) diffuseColor;
+			if (attribute==MaterialAttribute.OPACITY) return (T) Double.valueOf(opacity);
+			if (attribute==MaterialAttribute.SPECULARITY) return (T) Double.valueOf(specularity);
+			if (attribute==MaterialAttribute.DIFFUSE_TEXTURE_ID) return (@Nullable T) diffuseTextureId;
+			if (attribute==MaterialAttribute.SPECULAR_TEXTURE_ID) return (@Nullable T) specTextureId;
+			if (attribute==MaterialAttribute.NORMAL_TEXTURE_ID) return (@Nullable T) normalTextureId;
+			return null;
+		}
+		
+		public static Builder builder() {
+			return new Builder();
+		}
+		
+		public static class Builder {
+			private BlinnPhong result = new BlinnPhong();
+			
+			public Builder diffuseColor(int rgb) {
+				int r = (rgb >> 16) & 0xFF;
+				int g = (rgb >>  8) & 0xFF;
+				int b = (rgb      ) & 0xFF;
+				
+				result.diffuseColor.set(r/255.0, g/255.0, b/255.0);
+				
+				return this;
+			}
+			
+			public Builder diffuseColor(double r, double g, double b) {
+				result.diffuseColor.set(r, g, b);
+				return this;
+			}
+			
+			public Builder specularity(double spec) {
+				result.specularity = spec;
+				return this;
+			}
+			
+			public Builder opacity(double opacity) {
+				result.opacity = opacity;
+				return this;
+			}
+			
+			public Builder diffuseMap(String diffuseMap) {
+				result.diffuseTextureId = diffuseMap;
+				return this;
+			}
+			
+			public Builder specularMap(String specMap) {
+				result.specTextureId = specMap;
+				return this;
+			}
+			
+			public Builder normalMap(String normalMap) {
+				result.normalTextureId = normalMap;
+				return this;
+			}
+			
+			public BlinnPhong build() {
+				BlinnPhong r = result;
+				result = null; //Prevent the Builder from further edits on this material
+				return r;
+			}
+		}
+	}
+	
+	/*
+	public static class Generic implements Material {
+		private Map<MaterialAttribute<?>, Object> attributes = new HashMap<>();
+		
+		//TODO: Move to builder syntax
+		public <T> void set(MaterialAttribute<T> attribute, T t) {
+			attributes.put(attribute, t);
+		}
+		
+		@SuppressWarnings("unchecked")
+		@Override
+		public <T> @Nullable T getMaterialAttribute(MaterialAttribute<T> attribute) {
+			return (T) attributes.get(attribute);
+		}
+	}*/
+	
 }
--- src/main/java/com/playsawdust/chipper/glow/model/MaterialAttribute.java
+++ src/main/java/com/playsawdust/chipper/glow/model/MaterialAttribute.java
@@ -1,5 +1,8 @@
 package com.playsawdust.chipper.glow.model;
 
+import org.joml.Vector3dc;
+import org.joml.Vector4dc;
+
 /**
  * Represents some quality of a surface or a property at a vertex. For instance, UV location, diffuse color, or specularity.
  * Attributes could exist at either the vertex or material granularity. If a vertex value exists, and the pass supports per-vertex
@@ -8,7 +11,7 @@ package com.playsawdust.chipper.glow.model;
  * <p>The type parameter T is the immutable type for this attribute, returned from a direct query. For instance, a surface normal would be `Vec3di`.
  * <o>The type parameter U is the mutable type for this attribute, returned from a joml-style query. A surface normal would be `Vec3d`.
  */
-public class MaterialAttribute<T, U> {
+public class MaterialAttribute<T> {
 	private String name;
 	
 	public MaterialAttribute(String name) {
@@ -20,4 +23,74 @@ public class MaterialAttribute<T, U> {
 	public String toString() {
 		return "{ \"MaterialAttribute\": \""+name+"\" }";
 	}
+	
+	/* Texture map IDs for Blinn-Phong
+	 * Intended for Material use. Texture IDs shouldn't be used in vertex attributes unless you're submitting to a texarray pass or flattening them away with an atlas model.
+	 */
+	public static MaterialAttribute<String> DIFFUSE_TEXTURE_ID = new MaterialAttribute<>("diffuseTextureId");
+	public static MaterialAttribute<String> SPECULAR_TEXTURE_ID = new MaterialAttribute<>("specularTextureId");
+	public static MaterialAttribute<String> NORMAL_TEXTURE_ID = new MaterialAttribute<>("normalTextureId");
+	
+	
+	/*
+	 * Blinn-Phong attributes
+	 * This is a simple lighting model which basically any renderer/pass/shader will support.
+	 */
+	
+	public static MaterialAttribute<Vector3dc> DIFFUSE_COLOR = new MaterialAttribute<>("diffuseColor");
+	public static MaterialAttribute<Double> SPECULARITY = new MaterialAttribute<>("specularity");
+	
+	/** Not required. The surface normal in model-space.*/
+	public static MaterialAttribute<Vector3dc> NORMAL = new MaterialAttribute<>("normal");
+	
+	/**
+	 * Not required. A model-space vector 90 degrees away from the normal vector, pointing so that it will skim across ("tangent to") the surface left-to-right. Between the normal and
+	 * the tangent, a matrix can be constructed to translate back and forth between model-space (where {0,0,1} points towards the front of the model) and tangent-space
+	 * (where {0,0,1} points "up" out of the polygon). Since the incoming light and the camera vector are available in model-space or world-space, this enables normal maps
+	 * to be constructed in their "own" local space but used at different angles outside the XZ plane.
+	 */
+	public static MaterialAttribute<Vector3dc> TANGENT = new MaterialAttribute<>("tangent");
+	
+	/** Not required. This can be found from the crossproduct of the normal and tangent inside the vertex shader, so generally this shouldn't be used for material or vertex attributes. */
+	public static MaterialAttribute<Vector3dc> BITANGENT = new MaterialAttribute<>("bitangent");
+	
+	/** Not required. At 0 opacity, geometry is invisible. At 1 opacity, geometry is completely solid. Note that opacity can be controled at a finer-grained level using a diffuse texture. */
+	public static MaterialAttribute<Double> OPACITY = new MaterialAttribute<>("opacity");
+	
+	/*
+	 * PBR workflow attributes
+	 * These attributes aren't required, and often won't be provided. Authoring PBR materials can be very difficult, and not all renderers/passes/shaders support them.
+	 * But in the right hands, PBR materials give a surface an ineffable feeling of realism that simply can't be achieved in Blinn-Phong models.
+	 */
+	
+	/**
+	 * Albedo is distinct from diffuse color by having all shadows, AO, and surface detail subtracted out. As a result, albedo colors often look
+	 * really bright and washed-out. This can be hard to create assets for. Unless you're really intent on using a PBR workflow, use {@link #DIFFUSE_COLOR} instead.
+	 */
+	public static MaterialAttribute<Double> ALBEDO = new MaterialAttribute<>("albedo");
+	
+	/**
+	 * Low metalness represents a dielectric material with a diffuse color. High metalness represents a conductive material with high reflectivity,
+	 * but *no* diffuse color due to conservation of energy. In reality, optically, there are only full-dielectrics and full-conductors with different roughness and
+	 * index of refraction. If you just want a "make it shinier" knob that you don't have to think about, use {@link #SPECULARITY} instead.
+	 */
+	public static MaterialAttribute<Double> METALNESS = new MaterialAttribute<>("metalness");
+	
+	/**
+	 * Low smoothness scatters specular light, making reflections indistinct and causing diffuse interactions to be more pronounced. High smoothness creates a crisp mirror
+	 * finish where specular interactions dominate.
+	 */
+	public static MaterialAttribute<Double> SMOOTHNESS = new MaterialAttribute<>("smoothness");
+	
+	/**
+	 * Index of Refraction controls the light which passes through the material's surface, including diffuse light which gets bounced back out after a negligible distance.
+	 * How much light gets bent depends on both the IOR of the material the light is *exiting* and the IOR it's *entering*. If the IORs are the same, the light is not
+	 * bent. This is a useless slider for metals, where refracted light is absorbed.
+	 */
+	public static MaterialAttribute<Double> INDEX_OF_REFRACTION = new MaterialAttribute<>("indexOfRefraction");
+	
+	public static MaterialAttribute<String> ALBEDO_TEXTURE_ID = new MaterialAttribute<>("albedoTextureId");
+	public static MaterialAttribute<String> METALNESS_TEXTURE_ID = new MaterialAttribute<>("metalnessTextureId");
+	public static MaterialAttribute<String> SMOOTHNESS_TEXTURE_ID = new MaterialAttribute<>("smoothnessTextureId");
+	
 }
