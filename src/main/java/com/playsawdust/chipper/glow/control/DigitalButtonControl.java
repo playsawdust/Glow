af0c07e762dc1e69adc785b6bd640bdb040a3d67
Falkreon <falkreon@gmail.com>
2020-08-20 20:53:09 -0500

    Add some mouse and keyboard control helpers

---
+++ src/main/java/com/playsawdust/chipper/glow/control/DigitalButtonControl.java
@@ -0,0 +1,102 @@
+package com.playsawdust.chipper.glow.control;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.lwjgl.glfw.GLFW;
+
+import com.google.common.collect.ImmutableSet;
+
+public class DigitalButtonControl {
+	private HashMap<Integer, Boolean> keys = new HashMap<>();
+	private HashMap<Integer, Boolean> codes = new HashMap<>();
+	private boolean pressed = false;
+	private boolean locked = false;
+	
+	public void handle(int key, int code, int action, int mods) {
+		if (action!=GLFW.GLFW_PRESS && action!=GLFW.GLFW_RELEASE) return; //We won't properly handle non-press non-release actions here
+		
+		boolean press = (action==GLFW.GLFW_PRESS);
+		
+		if (keys.containsKey(key)) {
+			keys.put(key, press);
+		}
+		
+		if (codes.containsKey(code)) {
+			codes.put(code, press);
+		}
+		
+		//Go through what looks like a complicated process to resolve the combined state of this Control. Because of the low iteration count, it's very fast.
+		for(Map.Entry<Integer, Boolean> entry : keys.entrySet()) {
+			if (entry.getValue()) {
+				pressed = true;
+				return;
+			}
+		}
+		
+		for(Map.Entry<Integer, Boolean> entry : codes.entrySet()) {
+			if (entry.getValue()) {
+				pressed = true;
+				return;
+			}
+		}
+		
+		pressed = false;
+		locked = false;
+	}
+	
+	public boolean isPressed() {
+		return pressed;
+	}
+	
+	public boolean isActive() {
+		return pressed & !locked;
+	}
+	
+	/** Lock this key out from becoming active again until all keybinds are simultaneously unpressed. */
+	public void lock() {
+		locked = true;
+	}
+	
+	public Set<Integer> getKeys() {
+		return ImmutableSet.copyOf(keys.keySet());
+	}
+	
+	public Set<Integer> getScanCodes() {
+		return ImmutableSet.copyOf(codes.keySet());
+	}
+	
+	public void addKey(int key) {
+		keys.put(key, false);
+	}
+	
+	public void addCode(int code) {
+		codes.put(code, false);
+	}
+	
+	public void clearBindings() {
+		keys.clear();
+		codes.clear();
+	}
+	
+	/** Clears any existing mappings for this Control and bind to the named GLFW key-constant */
+	public void rebindToKey(int key) {
+		clearBindings();
+		addKey(key);
+	}
+	
+	/** Gets a Control that responds by default to the named GLFW key-constant. */
+	public static DigitalButtonControl forKey(int key) {
+		DigitalButtonControl control = new DigitalButtonControl();
+		control.keys.put(key, false);
+		return control;
+	}
+	
+	/** Gets a Control that responds to a system-dependent scancode. Use {@link org.lwjgl.glfw.GLFW#glfwGetKeyScancode(int)} to find the scanCode for a named key-constant */
+	public static DigitalButtonControl forScanCode(int code) {
+		DigitalButtonControl control = new DigitalButtonControl();
+		control.codes.put(code, false);
+		return control;
+	}
+}
---
+++ src/main/java/com/playsawdust/chipper/glow/control/MouseLook.java
@@ -0,0 +1,52 @@
+package com.playsawdust.chipper.glow.control;
+
+import org.joml.Matrix3d;
+import org.joml.Matrix3dc;
+import org.joml.Vector3d;
+
+public class MouseLook {
+	private static final double TAU = Math.PI*2;
+	private static final double QUARTER_TAU = Math.PI/2;
+	
+	private double lastX = Double.NaN;
+	private double lastY = Double.NaN;
+	
+	private double xRot = 0;
+	private double yRot = 0;
+	private double xSensitivity = 0.001; //TODO: Relative to window dimensions?
+	private double ySensitivity = 0.001;
+	private Matrix3d matrix = new Matrix3d();
+	
+	public Matrix3dc step(double mouseX, double mouseY, double winWidth, double winHeight) {
+		if (Double.isNaN(lastX)) lastX = mouseX;
+		if (Double.isNaN(lastY)) lastY = mouseY;
+		double dx = mouseX - lastX;
+		double dy = mouseY - lastY;
+		lastX = mouseX;
+		lastY = mouseY;
+		
+		yRot += dx * xSensitivity;
+		xRot += dy * ySensitivity;
+		
+		if (xRot >  QUARTER_TAU) xRot =  QUARTER_TAU;
+		if (xRot < -QUARTER_TAU) xRot = -QUARTER_TAU;
+		
+		matrix.identity();
+		matrix.rotate(xRot, 1, 0, 0);
+		matrix.rotate(yRot, 0, 1, 0);
+		
+		return matrix;
+	}
+	
+	public Matrix3dc getMatrix() {
+		return matrix;
+	}
+	
+	public Vector3d getLookVector(Vector3d result) {
+		if (result==null) result = new Vector3d();
+		
+		result.set(0, 0, -1);
+		new Matrix3d(matrix).invert().transform(result);
+		return result;
+	}
+}
